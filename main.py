8500b7ffa212d174f9a326dea85a5c1df0b3cfc7
julian <julianveraz@gmail.com>
2026-02-27 17:53:06 -0300

    Primer commit: subiendo proyecto

---
+++ main.py
@@ -0,0 +1,50 @@
+import os
+from typing import List, Dict, Any, Optional
+from datetime import datetime
+import psycopg2 
+from psycopg2.extras import RealDictCursor
+from fastmcp import FastMCP
+
+app = FastMCP("company-db-server")
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.environ.get("DB_HOST", "localhost"),
+        port= int(os.environ.get("DB_PORT")),
+        user=os.environ.get("DB_USER", "postgres"),
+        password=os.environ.get("DB_PASSWORD"),        
+        database=os.environ.get("DB_NAME", "company_db"),
+        cursor_factory = RealDictCursor
+    )
+    return conn 
+
+@app.tool
+def list_tracks(limit: int = 5) -> List[Dict[str, Any]]:
+    """Listar los tracks disponibles en la base de datos"""
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("" \
+        "SELECT AlbumId, Name, Composer FROM Track ORDER BY Composer LIMIT %s", 
+        (limit,))
+
+        rows = cursor.fetchall()
+        tracks = []
+        for row in rows:
+            tracks.append({
+                "album_id": row["albumid"], 
+                "name": row["name"],
+                "composer": row["composer"]
+            })       
+        cursor.close()
+        conn.close()
+
+        return tracks
+
+    except Exception as e:
+        return {
+            "error": f'Error al obtener los tracks: {str(e)}'
+        }
+    
+if __name__ == "__main__":
+    app.run(transport="sse", host="0.0.0.0", port=3000)
